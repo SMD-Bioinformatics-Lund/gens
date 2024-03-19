9f72952352fa21280fac29dd468bd3f59830dbf3
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2024-03-19 08:54:55 +0100

    Adjust coordinate comp to visible, bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="gens",
-    version="3.0.0",
+    version="3.0.1",
     description="Gens is a web-based interactive tool to visualize genomic copy number profiles from WGS data.",
     license="MIT",
     author="Ronja Grosz, Markus Johansson",
