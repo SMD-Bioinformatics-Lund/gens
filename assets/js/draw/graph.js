e26c71b989354c5501d739ffa0a5132e32480633
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2024-10-25 16:27:58 +0200

    Merge

--- assets/js/draw/graph.js
+++ assets/js/draw/graph.js
@@ -1,39 +1,53 @@
 // graph related objects
-import { drawRect, drawLine, drawRotatedText, drawText } from './shapes.js'
+import { drawRect, drawLine, drawRotatedText, drawText } from "./shapes.js";
 
 // Draws vertical tick marks for selected values between
 // xStart and xEnd with step length.
 // The amplitude scales the values to drawing size
-export function drawVerticalTicks ({
-  ctx, renderX, y, xStart, xEnd,
-  xoStart, xoEnd,
-  width, yMargin, titleColor
+export function drawVerticalTicks({
+  ctx,
+  renderX,
+  y,
+  xStart,
+  xEnd,
+  xoStart,
+  xoEnd,
+  width,
+  yMargin,
+  titleColor,
 }) {
-  const lineThickness = 1
-  const lineWidth = 5
-  const regionSize = xEnd - xStart
-  const scale = width / regionSize
-  const maxNumTicks = 30
+  const lineThickness = 1;
+  const lineWidth = 5;
+  const regionSize = xEnd - xStart;
+  const scale = width / regionSize;
+  const maxNumTicks = 30;
 
   // Create a step size which is an even power of ten (10, 100, 1000 etc)
-  let stepLength = 10 ** Math.floor(Math.log10(regionSize) - 1)
+  let stepLength = 10 ** Math.floor(Math.log10(regionSize) - 1);
 
   // Change to "half-steps" (50, 500, 5000) if too many ticks
   if (regionSize / stepLength > maxNumTicks) {
-    stepLength *= 5
+    stepLength *= 5;
   }
 
   // Get  starting position for the first tick
-  const xFirstTick = Math.ceil(xoStart / stepLength) * stepLength
+  const xFirstTick = Math.ceil(xoStart / stepLength) * stepLength;
 
   // Draw the ticks
   for (let step = xFirstTick; step <= xoEnd; step += stepLength) {
-    const xStep = Math.round(scale * (step - xoStart))
-    const value = numberWithCommas(step.toFixed(0))
+    const xStep = Math.round(scale * (step - xoStart));
+    const value = numberWithCommas(step.toFixed(0));
 
     // Draw text and ticks only for the leftmost box
-    drawRotatedText(ctx, value, 10, renderX + xStep + 8,
-      y - value.length - 3 * yMargin, -Math.PI / 4, titleColor)
+    drawRotatedText(
+      ctx,
+      value,
+      10,
+      renderX + xStep + 8,
+      y - value.length - 3 * yMargin,
+      -Math.PI / 4,
+      titleColor,
+    );
 
     // Draw tick line
     drawLine({
@@ -43,52 +57,95 @@ export function drawVerticalTicks ({
       x2: renderX + xStep,
       y2: y,
       lineWidth: lineThickness,
-      color: '#777'
-    })
+      color: "#777",
+    });
   }
 }
 
 // Draws horizontal lines for selected values between
 // yStart and yEnd with step length.
 // The amplitude scales the values to drawing size
-export function drawGraphLines ({ ctx, x, y, yStart, yEnd, stepLength, yMargin, width, height }) {
-  const ampl = (height - 2 * yMargin) / (yStart - yEnd) // Amplitude for scaling y-axis to fill whole height
-  const lineThickness = 1
+export function drawGraphLines({
+  ctx,
+  x,
+  y,
+  yStart,
+  yEnd,
+  stepLength,
+  yMargin,
+  width,
+  height,
+}) {
+  const ampl = (height - 2 * yMargin) / (yStart - yEnd); // Amplitude for scaling y-axis to fill whole height
+  const lineThickness = 1;
 
   for (let step = yStart; step >= yEnd; step -= stepLength) {
     // Draw horizontal line
-    const yPos = y + yMargin + (yStart - step) * ampl
+    const yPos = y + yMargin + (yStart - step) * ampl;
     drawLine({
       ctx,
       x,
       y: yPos,
       x2: x + width - 2 * lineThickness,
       y2: yPos,
-      color: '#e5e5e5'
-    })
+      color: "#e5e5e5",
+    });
   }
 }
 
 // Creates a graph for one chromosome data type
-export function createGraph (ctx, x, y, width, height, yMargin, yStart,
-  yEnd, step, addTicks, color = 'black', open) {
+export function createGraph(
+  ctx,
+  x,
+  y,
+  width,
+  height,
+  yMargin,
+  yStart,
+  yEnd,
+  step,
+  addTicks,
+  color = "black",
+  open,
+) {
   // Draw tick marks
   if (addTicks) {
-    drawTicks(ctx, x, y + yMargin, yStart, yEnd, step, yMargin, width,
-      height, color)
+    drawTicks(
+      ctx,
+      x,
+      y + yMargin,
+      yStart,
+      yEnd,
+      step,
+      yMargin,
+      width,
+      height,
+      color,
+    );
   }
 
   // Draw surrounding coordinate box
-  drawRect({ ctx, x, y, width, height, lineWidth: 1, color, open })
+  drawRect({ ctx, x, y, width, height, lineWidth: 1, color, open });
 }
 
 // Draws tick marks for selected values between
 // yStart and yEnd with step length.
 // The amplitude scales the values to drawing size
-function drawTicks (ctx, x, y, yStart, yEnd, stepLength, yMargin, width, height, color = 'black') {
-  const ampl = (height - 2 * yMargin) / (yStart - yEnd) // Amplitude for scaling y-axis to fill whole height
-  const lineThickness = 2
-  const lineWidth = 4
+function drawTicks(
+  ctx,
+  x,
+  y,
+  yStart,
+  yEnd,
+  stepLength,
+  yMargin,
+  width,
+  height,
+  color = "black",
+) {
+  const ampl = (height - 2 * yMargin) / (yStart - yEnd); // Amplitude for scaling y-axis to fill whole height
+  const lineThickness = 2;
+  const lineWidth = 4;
 
   for (let step = yStart; step >= yEnd; step -= stepLength) {
     drawText({
@@ -97,8 +154,8 @@ function drawTicks (ctx, x, y, yStart, yEnd, stepLength, yMargin, width, height,
       y: y + (yStart - step) * ampl + 2.2,
       text: step.toFixed(1),
       textSize: 10,
-      align: 'right'
-    })
+      align: "right",
+    });
 
     // Draw tick line
     drawLine({
@@ -108,12 +165,12 @@ function drawTicks (ctx, x, y, yStart, yEnd, stepLength, yMargin, width, height,
       x2: x,
       y2: y + (yStart - step) * ampl,
       lineWidth: lineThickness,
-      color
-    })
+      color,
+    });
   }
 }
 
 // Makes large numbers more readable with commas
-function numberWithCommas (x) {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
--- assets/js/track/variant.js
+++ assets/js/track/variant.js
@@ -1,64 +1,96 @@
 // Variant track definition
 
-import { BaseAnnotationTrack } from './base.js'
-import { isElementOverlapping, isWithinElementBbox, isWithinElementVisibleBbox } from './utils.js'
-import { drawRect, drawLine, drawWaveLine, drawText } from '../draw.js'
-import { initTrackTooltips, createTooltipElement, makeVirtualDOMElement, updateVisibleElementCoordinates } from './tooltip.js'
-import { createPopper } from '@popperjs/core'
+import { BaseAnnotationTrack } from "./base.js";
+import {
+  isElementOverlapping,
+  isWithinElementBbox,
+  isWithinElementVisibleBbox,
+} from "./utils.js";
+import { drawRect, drawLine, drawWaveLine, drawText } from "../draw.js";
+import {
+  initTrackTooltips,
+  createTooltipElement,
+  makeVirtualDOMElement,
+  updateVisibleElementCoordinates,
+} from "./tooltip.js";
+import { createPopper } from "@popperjs/core";
 
 // Draw variants
-const VARIANT_TR_TABLE = { del: 'deletion', dup: 'duplication', cnv: 'copy number variation', inv: 'inversion', bnd: 'break end' }
+const VARIANT_TR_TABLE = {
+  del: "deletion",
+  dup: "duplication",
+  cnv: "copy number variation",
+  inv: "inversion",
+  bnd: "break end",
+};
 
 export class VariantTrack extends BaseAnnotationTrack {
-  constructor (x, width, near, far, caseId, genomeBuild, colorSchema, scoutBaseURL, highlightedVariantId) {
+  constructor(
+    x,
+    width,
+    near,
+    far,
+    caseId,
+    genomeBuild,
+    colorSchema,
+    scoutBaseURL,
+    highlightedVariantId,
+  ) {
     // Dimensions of track canvas
-    const visibleHeight = 100 // Visible height for expanded canvas, overflows for scroll
-    const minHeight = 35 // Minimized height
+    const visibleHeight = 100; // Visible height for expanded canvas, overflows for scroll
+    const minHeight = 35; // Minimized height
 
-    super(width, near, far, visibleHeight, minHeight, colorSchema)
+    super(width, near, far, visibleHeight, minHeight, colorSchema);
 
     // Set inherited variables
-    this.drawCanvas = document.getElementById('variant-draw')
-    this.contentCanvas = document.getElementById('variant-content')
-    this.trackTitle = document.getElementById('variant-titles')
-    this.trackContainer = document.getElementById('variant-track-container')
-    this.scoutBaseURL = scoutBaseURL
+    this.drawCanvas = document.getElementById("variant-draw");
+    this.contentCanvas = document.getElementById("variant-content");
+    this.trackTitle = document.getElementById("variant-titles");
+    this.trackContainer = document.getElementById("variant-track-container");
+    this.scoutBaseURL = scoutBaseURL;
     // Add click menu event listener linking out to the Scout variant
-    this.trackContainer.addEventListener('click', async (event) => {
-      for (const element of this.geneticElements) {
-        const rect = this.contentCanvas.getBoundingClientRect()
-        const point = { x: (event.clientX - rect.left), y: event.clientY - rect.top }
-        if (isWithinElementVisibleBbox({ element, point })) {
-          const url = this.scoutBaseURL + '/document_id/' + element.id
-          console.log(`Visit ${url}: Scout variant`)
-          const win = window.open(url, '_blank')
-          win.focus()
+    this.trackContainer.addEventListener(
+      "click",
+      async (event) => {
+        for (const element of this.geneticElements) {
+          const rect = this.contentCanvas.getBoundingClientRect();
+          const point = {
+            x: event.clientX - rect.left,
+            y: event.clientY - rect.top,
+          };
+          if (isWithinElementVisibleBbox({ element, point })) {
+            const url = this.scoutBaseURL + "/document_id/" + element.id;
+            console.log(`Visit ${url}: Scout variant`);
+            const win = window.open(url, "_blank");
+            win.focus();
+          }
         }
-      }
-    }, false)
-    this.featureHeight = 18
+      },
+      false,
+    );
+    this.featureHeight = 18;
     // Setup html objects now that we have gotten the canvas and div elements
-    this.setupHTML(x + 1)
+    this.setupHTML(x + 1);
 
-    this.trackContainer.style.marginTop = '-1px'
-    this.genomeBuild = genomeBuild
+    this.trackContainer.style.marginTop = "-1px";
+    this.genomeBuild = genomeBuild;
 
     // GENS api parameters
-    this.apiEntrypoint = 'get-variant-data'
+    this.apiEntrypoint = "get-variant-data";
     this.additionalQueryParams = {
-      variant_category: 'sv',
-      case_id: caseId
-    }
+      variant_category: "sv",
+      case_id: caseId,
+    };
     // Initialize highlighted variant
-    this.highlightedVariantId = highlightedVariantId
-    initTrackTooltips(this)
+    this.highlightedVariantId = highlightedVariantId;
+    initTrackTooltips(this);
 
     // Initialize label tracking
-    this.labelData = []
+    this.labelData = [];
   }
 
   // Draw highlight for a given region
-  drawHighlight (startPos, endPos, color = 'rgb(255, 200, 87, 0.5)') {
+  drawHighlight(startPos, endPos, color = "rgb(255, 200, 87, 0.5)") {
     drawRect({
       ctx: this.drawCtx,
       x: startPos,
@@ -67,152 +99,166 @@ export class VariantTrack extends BaseAnnotationTrack {
       height: this.visibleHeight,
       lineWidth: 0,
       fillColor: color,
-      open: false
-    })
+      open: false,
+    });
   }
 
-  async drawOffScreenTrack ({ startPos, endPos, maxHeightOrder, data }) {
+  async drawOffScreenTrack({ startPos, endPos, maxHeightOrder, data }) {
     //  Draws variants in given range
-    const textSize = 10
+    const textSize = 10;
     // store positions used when rendering the canvas
     this.offscreenPosition = {
       start: startPos,
       end: endPos,
-      scale: this.drawCanvas.width /
-        (endPos - startPos)
-    }
-    const scale = this.offscreenPosition.scale
+      scale: this.drawCanvas.width / (endPos - startPos),
+    };
+    const scale = this.offscreenPosition.scale;
 
     // Set needed height of visible canvas and transcript tooltips
-    this.setContainerHeight(maxHeightOrder)
+    this.setContainerHeight(maxHeightOrder);
 
     // Keeps track of previous values
     this.heightOrderRecord = {
       latestHeight: 0, // Latest height order for annotation
       latestNameEnd: 0, // Latest annotations end position
-      latestTrackEnd: 0 // Latest annotations title's end position
-    }
+      latestTrackEnd: 0, // Latest annotations title's end position
+    };
 
     // limit drawing of annotations to pre-defined resolutions
-    let filteredVariants = []
+    let filteredVariants = [];
     if (this.getResolution < this.maxResolution + 1) {
-      filteredVariants = data
-        .variants
-        .filter(variant => isElementOverlapping(
+      filteredVariants = data.variants.filter((variant) =>
+        isElementOverlapping(
           { start: variant.position, end: variant.end },
-          { start: startPos, end: endPos }))
+          { start: startPos, end: endPos },
+        ),
+      );
     }
-    filteredVariants.sort((a, b) => a.position - b.position)
+    filteredVariants.sort((a, b) => a.position - b.position);
 
-    let heightTracker = Array(200)
-    let actualMaxHeightOrder = 1
+    let heightTracker = Array(200);
+    let actualMaxHeightOrder = 1;
     for (const variant of filteredVariants) {
-      const variantCategory = variant.sub_category // del, dup, sv, str
-      if (['dup', 'del', 'cnv'].includes(variantCategory)) {
-        let heightOrder = 1
-        while (heightTracker[heightOrder] >= variant.position)
-          heightOrder += 1
-        heightTracker[heightOrder] = variant.end
-        actualMaxHeightOrder = Math.max(actualMaxHeightOrder, heightOrder)
+      const variantCategory = variant.sub_category; // del, dup, sv, str
+      if (["dup", "del", "cnv"].includes(variantCategory)) {
+        let heightOrder = 1;
+        while (heightTracker[heightOrder] >= variant.position) heightOrder += 1;
+        heightTracker[heightOrder] = variant.end;
+        actualMaxHeightOrder = Math.max(actualMaxHeightOrder, heightOrder);
       }
     }
-    
-    this.trackData.max_height_order = actualMaxHeightOrder
+
+    this.trackData.max_height_order = actualMaxHeightOrder;
 
     // dont show tracks with no data in them
-    if (filteredVariants.length > 0 &&
-         this.getResolution < this.maxResolution + 1
+    if (
+      filteredVariants.length > 0 &&
+      this.getResolution < this.maxResolution + 1
     ) {
-      this.setContainerHeight(this.trackData.max_height_order)
+      this.setContainerHeight(this.trackData.max_height_order);
     } else {
-      this.setContainerHeight(0)
+      this.setContainerHeight(0);
     }
-    this.clearTracks()
+    this.clearTracks();
 
-    heightTracker = Array(200)
+    heightTracker = Array(200);
 
-    const labelData = []
+    const labelData = [];
 
     // Draw track
-    const drawTooltips = this.getResolution < 4
+    const drawTooltips = this.getResolution < 4;
     for (const variant of filteredVariants) {
-      const variantCategory = variant.sub_category // del, dup, sv, str
-      const variantType = variant.variant_type
-      const variantLength = variant.length
-      const color = this.colorSchema[variantCategory] || this.colorSchema.default || 'black'
-      
-      let heightOrder = 1
-      if (['dup', 'del', 'cnv'].includes(variantCategory)) {
-        while (heightTracker[heightOrder] >= variant.position)
-          heightOrder += 1
-        heightTracker[heightOrder] = variant.end
+      const variantCategory = variant.sub_category; // del, dup, sv, str
+      const variantType = variant.variant_type;
+      const variantLength = variant.length;
+      const color =
+        this.colorSchema[variantCategory] ||
+        this.colorSchema.default ||
+        "black";
+
+      let heightOrder = 1;
+      if (["dup", "del", "cnv"].includes(variantCategory)) {
+        while (heightTracker[heightOrder] >= variant.position) heightOrder += 1;
+        heightTracker[heightOrder] = variant.end;
       }
 
-      const canvasYPos = this.tracksYPos(heightOrder)
+      const canvasYPos = this.tracksYPos(heightOrder);
 
       // Only draw visible tracks
-      if (!this.expanded && heightOrder !== 1) { continue }
+      if (!this.expanded && heightOrder !== 1) {
+        continue;
+      }
 
       // create variant object
-      const featureHeight = variantCategory === 'del' ? 7 : 8
+      const featureHeight = variantCategory === "del" ? 7 : 8;
       const variantObj = {
         id: variant.document_id,
         name: variant.display_name,
         start: variant.position,
         end: variant.end,
-        x1: Math.round(scale * (variant.position - this.offscreenPosition.start)),
+        x1: Math.round(
+          scale * (variant.position - this.offscreenPosition.start),
+        ),
         x2: Math.round(scale * (variant.end - this.offscreenPosition.start)),
         y1: canvasYPos,
-        y2: Math.round((canvasYPos + featureHeight)),
+        y2: Math.round(canvasYPos + featureHeight),
         features: [],
         isDisplayed: false,
         tooltip: false,
-      }
+      };
       // get onscreen positions for offscreen xy coordinates
       updateVisibleElementCoordinates({
         element: variantObj,
         screenPosition: this.onscreenPosition,
-        scale: this.offscreenPosition.scale
-      })
+        scale: this.offscreenPosition.scale,
+      });
       // create a tooltip html element and append to DOM
-      if (drawTooltips && ['dup', 'del', 'cnv'].includes(variantCategory)) {
+      if (drawTooltips && ["dup", "del", "cnv"].includes(variantCategory)) {
         const tooltip = createTooltipElement({
           id: `popover-${variantObj.id}`,
           title: `${variantType.toUpperCase()}: ${variant.category} - ${VARIANT_TR_TABLE[variantCategory]}`,
           information: [
-            { title: 'Type', value: variant.category },
+            { title: "Type", value: variant.category },
             { title: variant.chromosome, value: `${variant.position}` },
-            { title: 'Ref', value: `${variant.reference}` },
-            { title: 'Alt', value: `${variant.alternative}` },
-            { title: 'Cytoband start/end', value: `${variant.cytoband_start}/${variant.cytoband_end}` },
-            { title: 'Length', value: `${variant.length}` },
-            { title: 'Quality', value: `${variant.quality}` },
-            { title: 'Rank score', value: `${variant.rank_score}` }
-          ]
-        })
-        this.trackContainer.appendChild(tooltip)
+            { title: "Ref", value: `${variant.reference}` },
+            { title: "Alt", value: `${variant.alternative}` },
+            {
+              title: "Cytoband start/end",
+              value: `${variant.cytoband_start}/${variant.cytoband_end}`,
+            },
+            { title: "Length", value: `${variant.length}` },
+            { title: "Quality", value: `${variant.quality}` },
+            { title: "Rank score", value: `${variant.rank_score}` },
+          ],
+        });
+        this.trackContainer.appendChild(tooltip);
         // make a  virtual element as tooltip hitbox
         const virtualElement = makeVirtualDOMElement({
           x1: variantObj.visibleX1,
           x2: variantObj.visibleX2,
           y1: variantObj.visibleY1,
           y2: variantObj.visibleY2,
-          canvas: this.contentCanvas
-        })
+          canvas: this.contentCanvas,
+        });
         // add tooltip to variantObj
         variantObj.tooltip = {
           instance: createPopper(virtualElement, tooltip, {
             modifiers: [
-              { name: 'offset', options: { offset: [0, virtualElement.getBoundingClientRect().height] } }
-            ]
+              {
+                name: "offset",
+                options: {
+                  offset: [0, virtualElement.getBoundingClientRect().height],
+                },
+              },
+            ],
           }),
           virtualElement: virtualElement,
           tooltip: tooltip,
-          isDisplayed: false
-        }
+          isDisplayed: false,
+        };
       }
-      if (['dup', 'del', 'cnv'].includes(variantCategory)) {
-        this.geneticElements.push(variantObj)
+      if (["dup", "del", "cnv"].includes(variantCategory)) {
+        this.geneticElements.push(variantObj);
       }
 
       // Keep track of latest track
@@ -220,44 +266,44 @@ export class VariantTrack extends BaseAnnotationTrack {
         this.heightOrderRecord = {
           latestHeight: heightOrder,
           latestNameEnd: 0,
-          latestTrackEnd: 0
-        }
+          latestTrackEnd: 0,
+        };
       }
       // Draw motif line
       switch (variantCategory) {
-        case 'del':
+        case "del":
           drawWaveLine({
             ctx: this.drawCtx,
             x: variantObj.x1,
             y: variantObj.y2,
             x2: variantObj.x2,
             height: featureHeight,
-            color
-          })
-          break
-        case 'cnv':
-        case 'dup':
+            color,
+          });
+          break;
+        case "cnv":
+        case "dup":
           drawLine({
             ctx: this.drawCtx,
             x: variantObj.x1,
             y: variantObj.y1,
             x2: variantObj.x2,
             y2: variantObj.y1,
-            color
-          })
+            color,
+          });
           drawLine({
             ctx: this.drawCtx,
             x: variantObj.x1,
             y: variantObj.y2,
             x2: variantObj.x2,
             y2: variantObj.y2,
-            color
-          })
-          break
-        case 'bnd':
-        case 'inv':
+            color,
+          });
+          break;
+        case "bnd":
+        case "inv":
           // no support for balanced events
-          break
+          break;
         default: // other types of elements
           drawLine({
             ctx: this.drawCtx,
@@ -265,17 +311,19 @@ export class VariantTrack extends BaseAnnotationTrack {
             y: variantObj.y1,
             x2: variantObj.x2,
             y2: variantObj.y1,
-            color
-          })
-          console.log(`Unhandled variant type ${variantCategory}; drawing default shape`)
+            color,
+          });
+          console.log(
+            `Unhandled variant type ${variantCategory}; drawing default shape`,
+          );
       }
       // Move and display highlighted region
       if (variant._id === this.highlightedVariantId) {
-        this.drawHighlight(variantObj.x1, variantObj.x2)
+        this.drawHighlight(variantObj.x1, variantObj.x2);
       }
 
-      if (['dup', 'del', 'cnv'].includes(variantCategory)) {
-        const textYPos = this.tracksYPos(heightOrder)
+      if (["dup", "del", "cnv"].includes(variantCategory)) {
+        const textYPos = this.tracksYPos(heightOrder);
         // Draw variant type
         labelData.push({
           text: `${variant.category} - ${variantType} ${VARIANT_TR_TABLE[variantCategory]}; length: ${variantLength}`,
@@ -283,8 +331,8 @@ export class VariantTrack extends BaseAnnotationTrack {
           end: variantObj.end,
           x: (variantObj.start + variantObj.end) / 2,
           y: textYPos + this.featureHeight,
-          fontProp: textSize
-        })
+          fontProp: textSize,
+        });
         /* drawText({
           ctx: this.drawCtx,
           text: `${variant.category} - ${variantType} ${VARIANT_TR_TABLE[variantCategory]}; length: ${variantLength}`,
@@ -294,23 +342,34 @@ export class VariantTrack extends BaseAnnotationTrack {
         }) */
       }
     }
-    this.labelData = labelData
+    this.labelData = labelData;
   }
 
-  drawDynamicOverlay () {
-    const ctx = this.contentCanvas.getContext('2d')
-    const scale = this.contentCanvas.width / (this.onscreenPosition.end - this.onscreenPosition.start)
-    const margin = 100
+  drawDynamicOverlay() {
+    const ctx = this.contentCanvas.getContext("2d");
+    const scale =
+      this.contentCanvas.width /
+      (this.onscreenPosition.end - this.onscreenPosition.start);
+    const margin = 100;
     this.labelData.forEach((label) => {
-      if (label.start < this.onscreenPosition.end && label.end > this.onscreenPosition.start) {
+      if (
+        label.start < this.onscreenPosition.end &&
+        label.end > this.onscreenPosition.start
+      ) {
         drawText({
           ctx: ctx,
           text: label.text,
-          x: Math.max(margin, Math.min(this.contentCanvas.width - margin, scale * (label.x - this.onscreenPosition.start))),
+          x: Math.max(
+            margin,
+            Math.min(
+              this.contentCanvas.width - margin,
+              scale * (label.x - this.onscreenPosition.start),
+            ),
+          ),
           y: label.y,
-          fontProp: label.fontProp
-        })
+          fontProp: label.fontProp,
+        });
       }
-    })
+    });
   }
 }
