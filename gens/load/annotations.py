4fbd9343787be335276d4b4ea1c8d3f503f400ff
Jakob Willforss <jakob.willforss@hotmail.com>
2024-11-15 12:58:35 +0100

    Fix list of expected database config vars

--- gens/load/annotations.py
+++ gens/load/annotations.py
@@ -1,4 +1,5 @@
 """Annotations."""
+
 import csv
 import logging
 import re
@@ -19,27 +20,10 @@ class ParserError(Exception):
     pass
 
 
-def parse_bed(file, genome_build):
+def parse_bed(file, _genome_build):
     """Parse bed file."""
-    HEADER = (
-        "sequence",
-        "start",
-        "end",
-        "name",
-        "score",
-        "strand",
-        "null",
-        "null",
-        "color",
-    )
     with open(file) as bed:
-        bed_reader = csv.DictReader(bed, delimiter="\t")
-        # Skip bad header info
-        next(bed_reader)
-
-        # Load in annotations
-        for line in bed_reader:
-            yield dict(zip(HEADER, line))
+        return csv.DictReader(bed, delimiter="\t")
 
 
 def parse_aed(file):
