63c5718b0e93f40533fc07ff073f930506ac6320
Jakob Willforss <jakob.willforss@hotmail.com>
2025-02-07 11:33:57 +0100

    mypy setup

--- gens/auth.py
+++ gens/auth.py
@@ -1,6 +1,6 @@
 """Authentication and login related functions."""
 from authlib.integrations.flask_client import OAuth
-from flask_login import LoginManager
+from flask_login import LoginManager # type: ignore
 
 login_manager = LoginManager()
 oauth_client = OAuth()
