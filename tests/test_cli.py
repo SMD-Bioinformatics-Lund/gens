45ec97ac6e5d321ed50c23b4838f456c305cc0f6
Markus Johansson <markus.h.johansson@skane.se>
2025-02-25 10:03:58 +0100

    wip cli load annoatation tests

---
+++ tests/test_cli.py
@@ -0,0 +1,24 @@
+"""Test cli commands."""
+
+import pytest
+from pathlib import Path
+from click.testing import CliRunner
+from gens.commands.load import annotations
+
+@pytest.mark.parametrize('fixture_name,n_annotations,genome_build', [
+    ('aed_file_path', 2, 38), 
+])
+def test_load_annotation(fixture_name: str, n_annotations: int, genome_build: int, request):
+    """Test load annotation file to the database."""
+    # get annotation file path
+    input_file: Path = request.getfixturevalue(fixture_name)
+
+    # run cli command
+    runner = CliRunner()
+    args = ['--file', str(input_file), '--genome-build', str(genome_build)]
+    result = runner.invoke(annotations, args)
+
+    # Test that the command finished sucessfully
+    assert result.exit_code == 0
+
+    # Test that the correct number of records were added to the db
