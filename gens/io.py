fe617999b038c0fbe2af859e925c0fe31fc20cbb
Jakob Willforss <jakob.willforss@hotmail.com>
2025-03-04 14:09:30 +0100

    Remove dev log

--- gens/io.py
+++ gens/io.py
@@ -41,7 +41,6 @@ def tabix_query(
 
     # Get data from bed file
     record_name = f"{zoom_level.value}_{chrom.value}"
-    LOG.info("Query %s; %s %d %d; reduce: %d", tbix.filename, record_name, start, end, reduce)
     try:
         records = tbix.fetch(record_name, start, end)
     except ValueError as err:
