5f5744ce018ea9916433b9aa9fadaf6f9fa36d70
Jakob Willforss <jakob.willforss@hotmail.com>
2025-04-03 14:08:16 +0200

    Go back to 2-space tabs

--- assets/js/components/tracks/overview_track.ts
+++ assets/js/components/tracks/overview_track.ts
@@ -3,108 +3,104 @@ import { transformMap, padRange, rangeSize } from "../../track/utils";
 import { CanvasTrack } from "./canvas_track";
 import { drawDotsScaled, linearScale, renderBorder } from "./render_utils";
 
-
 const X_PAD = 5;
 const DOT_SIZE = 2;
 
 export class OverviewTrack extends CanvasTrack {
-    totalChromSize: number;
-    chromSizes: Record<string, number>;
+  totalChromSize: number;
+  chromSizes: Record<string, number>;
 
-    initialize(
-        label: string,
-        trackHeight: number,
-        chromSizes: Record<string, number>,
-    ) {
-        super.initializeCanvas(label, trackHeight);
-        this.chromSizes = chromSizes;
-    }
+  initialize(
+    label: string,
+    trackHeight: number,
+    chromSizes: Record<string, number>,
+  ) {
+    super.initializeCanvas(label, trackHeight);
+    this.chromSizes = chromSizes;
+  }
 
-    // What are the inputs?
-    render(
-        selectedChrom: string | null,
-        dotsPerChrom: Record<string, RenderDot[]>,
-        yRange: [number, number],
-        onChromosomeClick: (chrom: string) => void
-    ) {
-        super.syncDimensions();
+  // What are the inputs?
+  render(
+    selectedChrom: string | null,
+    dotsPerChrom: Record<string, RenderDot[]>,
+    yRange: [number, number],
+    onChromosomeClick: (chrom: string) => void,
+  ) {
+    super.syncDimensions();
 
-        const totalChromSize = Object.values(this.chromSizes).reduce(
-            (tot, size) => tot + size,
-            0,
-        );
+    const totalChromSize = Object.values(this.chromSizes).reduce(
+      (tot, size) => tot + size,
+      0,
+    );
 
-        renderBorder(this.ctx, this.dimensions);
+    renderBorder(this.ctx, this.dimensions);
 
-        const xScale = (pos: number) => {
-            return linearScale(
-                pos,
-                [0, totalChromSize],
-                [0, this.dimensions.width],
-            );
-        };
+    const xScale = (pos: number) => {
+      return linearScale(pos, [0, totalChromSize], [0, this.dimensions.width]);
+    };
 
-        const yScale = (pos: number) => {
-            return linearScale(pos, yRange, [0, this.dimensions.height]);
-        };
+    const yScale = (pos: number) => {
+      return linearScale(pos, yRange, [0, this.dimensions.height]);
+    };
 
-        const chromRanges = getChromRanges(this.chromSizes);
-        // Draw the initial lines
-        Object.values(chromRanges).forEach(([_chromStart, chromEnd]) =>
-            drawVerticalLine(this.ctx, chromEnd, xScale),
-        );
+    const chromRanges = getChromRanges(this.chromSizes);
+    // Draw the initial lines
+    Object.values(chromRanges).forEach(([_chromStart, chromEnd]) =>
+      drawVerticalLine(this.ctx, chromEnd, xScale),
+    );
 
-        const pxRanges: Record<string, Rng> = transformMap(
-            chromRanges,
-            ([start, end]) => [xScale(start), xScale(end)],
-        );
+    const pxRanges: Record<string, Rng> = transformMap(
+      chromRanges,
+      ([start, end]) => [xScale(start), xScale(end)],
+    );
 
-        Object.entries(dotsPerChrom).forEach(([chrom, dotData]) => {
-            const pad = X_PAD;
-            const pxRange = padRange(pxRanges[chrom], pad);
+    Object.entries(dotsPerChrom).forEach(([chrom, dotData]) => {
+      const pad = X_PAD;
+      const pxRange = padRange(pxRanges[chrom], pad);
 
-            const chromXScale = (pos: number) => {
-                return linearScale(pos, [0, this.chromSizes[chrom]], pxRange);
-            };
+      const chromXScale = (pos: number) => {
+        return linearScale(pos, [0, this.chromSizes[chrom]], pxRange);
+      };
 
-            drawDotsScaled(this.ctx, dotData, chromXScale, yScale, DOT_SIZE);
-        });
+      drawDotsScaled(this.ctx, dotData, chromXScale, yScale, DOT_SIZE);
+    });
 
-        this.canvas.addEventListener("mousedown", (event) => {
-            event.stopPropagation();
-            const chrom = pixelToChrom(event.offsetX, pxRanges);
-            onChromosomeClick(chrom);
-        })
-    }
+    this.canvas.addEventListener("mousedown", (event) => {
+      event.stopPropagation();
+      const chrom = pixelToChrom(event.offsetX, pxRanges);
+      onChromosomeClick(chrom);
+    });
+  }
 }
 
 function pixelToChrom(xPixel: number, pxRanges: Record<string, Rng>): string {
-    for (const [chrom, range] of Object.entries(pxRanges)) {
-        if (xPixel >= range[0] && xPixel < range[1]) {
-            return chrom
-        }
+  for (const [chrom, range] of Object.entries(pxRanges)) {
+    if (xPixel >= range[0] && xPixel < range[1]) {
+      return chrom;
     }
-    throw Error(`Something went wrong, no chromosome range matched position: ${xPixel}`);
+  }
+  throw Error(
+    `Something went wrong, no chromosome range matched position: ${xPixel}`,
+  );
 }
 
-
 function getChromRanges(
-    chromSizes: Record<string, number>,
+  chromSizes: Record<string, number>,
 ): Record<string, [number, number]> {
-    const chromRanges: Record<string, [number, number]> = {};
-    let sumPos = 0;
-    Object.entries(chromSizes).forEach(([chrom, chromLength]) => {
-        const startPos = sumPos;
-        sumPos += chromLength;
+  const chromRanges: Record<string, [number, number]> = {};
+  let sumPos = 0;
+  Object.entries(chromSizes).forEach(([chrom, chromLength]) => {
+    const startPos = sumPos;
+    sumPos += chromLength;
 
-        const posRange: Rng = [startPos, sumPos];
-        // const pxRange = posRange.map((pos) =>
-        //     scaleToPixels(pos, totalChromSize, screenWidth),
-        // ) as [number, number];
+    const posRange: Rng = [startPos, sumPos];
+    // const pxRange = posRange.map((pos) =>
+    //     scaleToPixels(pos, totalChromSize, screenWidth),
+    // ) as [number, number];
 
-        chromRanges[chrom] = posRange;
-    });
-    return chromRanges;
+    chromRanges[chrom] = posRange;
+  });
+  return chromRanges;
 }
 
 function drawSegmentDots(dots: RenderDot[], pxRange: [number, number]) {}
