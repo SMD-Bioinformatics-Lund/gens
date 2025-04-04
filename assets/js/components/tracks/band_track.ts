cb7826762bbd0895b5c9850cb8b2c461d72c9a6d
Jakob Willforss <jakob.willforss@hotmail.com>
2025-04-04 07:45:26 +0200

    Disable tooltip work for merge

--- assets/js/components/tracks/band_track.ts
+++ assets/js/components/tracks/band_track.ts
@@ -1,3 +1,5 @@
+import { createTooltipElement, makeVirtualDOMElement } from "../../track/tooltip";
+import { stringToHash } from "../../track/utils";
 import { CanvasTrack } from "./canvas_track";
 import { renderBands, renderBorder } from "./render_utils";
 
@@ -9,25 +11,56 @@ export class BandTrack extends CanvasTrack {
 
     render(xRange: [number, number], annotations: RenderBand[]) {
         const dimensions = super.syncDimensions();
-        console.log(`Rendering band track ${this.label} with dim.width: ${dimensions.width}`);
-
-        console.log("Canvas attr width:", this.canvas.width);
-        console.log("Canvas style width:", this.canvas.style.width);
-        console.log("Canvas clientWidth:", this.canvas.clientWidth);
-
-        console.log("Canvas attr height:", this.canvas.height);
-        console.log("Canvas clientHeight:", this.canvas.clientHeight);
         
-
         const annotWithinRange = annotations.filter(
             (annot) => annot.start >= xRange[0] && annot.end <= xRange[1],
         );
 
+        // Hover
+
         renderBorder(this.ctx, dimensions);
         renderBands(this.ctx, dimensions, annotWithinRange, xRange);
-    }
-}
 
-export function test() {}
+        // this.renderTooltip(annotWithinRange);
+    }
+
+    // renderTooltip(annotations: RenderBand[]) {
+    //     const tooltip = createTooltipElement({
+    //     id: "popover-${annotationObj.id}",
+    //     title: "annotationObj.name",
+    //     information: [
+    //         { title: "More info", value: "Value" }
+    //         // { title: track.chrom, value: `${track.start}-${track.end}` },
+    //         // { title: "Score", value: `${track.score}` },
+    //     ],
+    //     });
+    //     this.trackContainer.appendChild(tooltip);
+
+    //     for (const track of annotations) {
+    //         const annotationObj = {
+    //           id: stringToHash(
+    //             `${track.name}-${annotations.start}-${track.end}-${track.color}`
+    //           ),
+    //           name: "track.name",
+    //           start: track.start,
+    //           end: track.end,
+    //           x1: x1,
+    //           x2: x1 + scale * (end - start + 1),
+    //           y1: canvasYPos,
+    //           y2: canvasYPos + this.featureHeight / 2,
+    //           features: [],
+    //           isDisplayed: false,
+    //         } as DisplayElement;
+      
+    //           const virtualElement = makeVirtualDOMElement({
+    //           x1: annotationObj.visibleX1,
+    //           x2: annotationObj.visibleX2,
+    //           y1: annotationObj.visibleY1,
+    //           y2: annotationObj.visibleY2,
+    //           canvas: this.contentCanvas,
+    //           });
+    //     }
+    // }
+}
 
 customElements.define("band-track", BandTrack);
