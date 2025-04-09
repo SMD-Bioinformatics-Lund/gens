6d58aaac0eddb70946691727594bb98689e06dd2
Markus Johansson <markus.h.johansson@skane.se>
2025-04-09 15:35:04 +0200

    Transcript entrypoint repsects coordinates

--- gens/routes/transcripts.py
+++ gens/routes/transcripts.py
@@ -18,8 +18,10 @@ async def get_transcripts(chromosome: Chromosome, genome_build: GenomeBuild, db:
     
     # lookup end of chromosome if no end is defined and calculate zoom level
     start = start if start is not None else 1
-    chrom_info = get_chromosome_info(db, chromosome, genome_build)
+    if end is None:
+        chrom_info = get_chromosome_info(db, chromosome, genome_build)
+        end = chrom_info.size
 
     # get transcript for the new region
-    transcripts: list[TranscriptRecord] = crud_get_transcripts(chromosome, start, chrom_info.size , genome_build, db)
+    transcripts: list[TranscriptRecord] = crud_get_transcripts(chromosome, start, end , genome_build, db)
     return transcripts
