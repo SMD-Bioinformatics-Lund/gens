6c56dc2805adc23673bf0190ba2870afffecd9ac
Jakob Willforss <jakob.willforss@hotmail.com>
2025-05-13 10:57:00 +0200

    Iterating on using the AG community grid

--- assets/js/home/gens_home.ts
+++ assets/js/home/gens_home.ts
@@ -1,5 +1,18 @@
+// import Tabulator from "tabulator-tables";
+
 import { ShadowBaseElement } from "../components/util/shadowbaseelement";
 import { removeChildren } from "../util/utils";
+
+import {
+  ModuleRegistry,
+  AllCommunityModule,
+  createGrid,
+  GridOptions,
+} from "ag-grid-community";
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
+
+ModuleRegistry.registerModules([AllCommunityModule]);
 
 export interface SampleInfo {
   case_id: string;
@@ -10,66 +23,85 @@ export interface SampleInfo {
   created_at: string;
 }
 
+const tableBorderColor = "#ddd";
+const menubarBgColor = "#4c6d94";
+const menubarFontColor = "#f4faff";
+
 const tableTemplate = document.createElement("template");
 tableTemplate.innerHTML = String.raw`
-  <table>
-    <thead>
-      <tr>
-        <td>Case ID</td>
-        <td>Sample ID(s)</td>
-        <td>Genome build</td>
-        <td>Overview file(s)</td>
-        <td>Coverage/BAF(s)</td>
-        <td>Import date</td>
-      </tr>
-    </thead>
-    <tbody id="body">
-      <tr>
-        <td>HG002</td>
-        <td>1,2,3</td>
-        <td>38</td>
-        <td>OK</td>
-        <td>OK</td>
-        <td>Import date</td>
-      </tr>
-    </tbody>
-  </table>
+<link rel="stylesheet" href="https://unpkg.com/tabulator-tables/dist/css/tabulator.min.css"/>
+<div id="table-container" class="ag-theme-alpine"></div>
 `;
 
 export class SamplesTable extends ShadowBaseElement {
-  private bodyElem: HTMLBodyElement;
+  private tableContainer: HTMLDivElement;
+  // private tabulator?: Tabulator;
 
   constructor() {
     super(tableTemplate);
   }
 
   connectedCallback(): void {
-    console.log("Connecting");
-    this.bodyElem = this.root.querySelector("#body");
+    super.connectedCallback();
+
+    this.tableContainer = this.root.querySelector("#table-container");
+    const gridOptions = {
+      // Row Data: The data to be displayed.
+      rowData: [
+        { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+        { make: "Ford", model: "F-Series", price: 33850, electric: false },
+        { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+      ],
+      // Column Definitions: Defines the columns to be displayed.
+      columnDefs: [
+        { field: "make" },
+        { field: "model" },
+        { field: "price" },
+        { field: "electric" },
+      ],
+    };
+    // @ts-ignore
+    createGrid(this.tableContainer, gridOptions);
   }
 
   initialize(sampleInfo: SampleInfo[], scoutBaseURL: string) {
+    if (!this.isConnected) {
+      throw Error(
+        "The component needs to be connected before being initialized",
+      );
+    }
+
+    // if (this.tabulator) {
+    //   this.tabulator.replaceData(sampleInfo);
+    //   return;
+    // }
+
+    console.log("Setting things up with", this.tableContainer);
+
+    // this.tabulator = new Tabulator(this.tableContainer, {
+    //   data: sampleInfo,
+    //   layout: "fitColumns",
+    //   pagination: "local",
+    //   paginationSize: 50,
+    //   autoResize: false,
+    //   placeholder: "No samples found",
+    //   columns: [
+    //     { title: "Case ID", field: "case_id" },
+    //     { title: "Sample ID(s)", field: "sample_ids" },
+    //     { title: "Genome build", field: "genome_build" },
+    //     { title: "Overview file(s)", field: "has_overview_file" },
+    //     { title: "Coverage/BAF(s)", field: "files_present" },
+    //     { title: "Import date", field: "created_at" },
+    //   ],
+    // });
+  }
+
+  /* initialize(sampleInfo: SampleInfo[], scoutBaseURL: string) {
     console.log(this.bodyElem);
     removeChildren(this.bodyElem);
 
     for (const sample of sampleInfo) {
       const row = document.createElement("tr");
-
-      // case_id: string,
-      // sample_ids: string[],
-      // genome_build: number,
-      // has_overview_file: boolean,
-      // files_present: boolean,
-      // created_at: string,
-
-      // const fields = [
-      //   "case_id",
-      //   "sample_ids",
-      //   "genome_build",
-      //   "has_overview_file",
-      //   "files_present",
-      //   "created_at",
-      // ];
 
       // Case cell
       const caseCell = document.createElement("td");
@@ -115,7 +147,7 @@ export class SamplesTable extends ShadowBaseElement {
 
       this.bodyElem.appendChild(row);
     }
-  }
+  } */
 }
 
 customElements.define("samples-table", SamplesTable);
@@ -124,11 +156,13 @@ const template = document.createElement("template");
 template.innerHTML = String.raw`
   <style></style>
   <div class="content">
-      <h1>Samples</h1>
-      <div>There are <span id="nr-samples">X</span> samples loaded into Gens. Click on a <em>sample_id</em> to open it</div>
+    <h1>Samples</h1>
+    <div>There are <span id="nr-samples">X</span> samples loaded into Gens. Click on a <em>sample_id</em> to open it</div>
+    <div>
+      <div class="display-info">Displaying 1 to 20 of 3 samples</div>
+      <samples-table id="samples-table"></samples-table>
+    </div>
   </div>
-  <div class="display-info">Displaying 1 to 20 of 3 samples</div>
-  <samples-table id="samples-table"></samples-table>
 `;
 
 export class GensHome extends HTMLElement {
