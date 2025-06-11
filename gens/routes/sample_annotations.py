9e66b4bb741a17c3a780600e062cce1034d4e31b
Jakob Willforss <jakob.willforss@hotmail.com>
2025-06-11 16:25:44 +0200

    Clicking sample annotation bands now runs

--- gens/routes/sample_annotations.py
+++ gens/routes/sample_annotations.py
@@ -1,13 +1,15 @@
-from fastapi import APIRouter
+from http import HTTPStatus
+from fastapi import APIRouter, HTTPException
 
 from gens.crud.sample_annotations import (
     get_sample_annotation_tracks,
+    get_sample_annotations,
     get_sample_annotations_for_track,
 )
 from gens.models.annotation import SimplifiedTrackInfo
 from gens.models.base import PydanticObjectId
 from gens.models.genomic import Chromosome, GenomeBuild
-from gens.models.sample_annotation import SampleAnnotationTrackInDb
+from gens.models.sample_annotation import SampleAnnotationRecord, SampleAnnotationTrackInDb
 from gens.routes.utils import ApiTags, GensDb
 
 
@@ -32,3 +34,13 @@ async def get_sample_annotations_route(
     track_id: PydanticObjectId, chromosome: Chromosome, db: GensDb
 ) -> list[SimplifiedTrackInfo]:
     return get_sample_annotations_for_track(track_id=track_id, chromosome=chromosome, db=db)
+
+
+@router.get("/annotations/record/{record_id}", tags=[ApiTags.SAMPLE_ANNOT])
+async def get_sample_annotation_record_route(
+    record_id: PydanticObjectId, db: GensDb,
+) -> SampleAnnotationRecord:
+    result = get_sample_annotations(record_id, db)
+    if result is None:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND)
+    return result
