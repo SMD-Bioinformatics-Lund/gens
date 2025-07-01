ce0b9317445efc477833eb27ea8a8d4d57f7441b
Jakob Willforss <jakob.willforss@hotmail.com>
2025-07-01 08:27:20 +0200

    Remove FlaskGroup dependency

--- gens/cli/base.py
+++ gens/cli/base.py
@@ -1,10 +1,10 @@
 """Gens command line interface."""
 
 import click
-from flask.cli import FlaskGroup
+# from flask.cli import FlaskGroup
 
 from gens.__version__ import VERSION as version
-from gens.app import create_app
+# from gens.app import create_app
 
 from .delete import delete as delete_command
 from .update import update as update_command
@@ -12,12 +12,7 @@ from .index import index as index_command
 from .load import load as load_command
 
 
-@click.group(
-    cls=FlaskGroup,
-    create_app=create_app,
-    add_default_commands=False,
-    add_version_option=False,
-)
+@click.group()
 @click.version_option(version)
 def cli() -> None:
     """Management of Gens application"""
