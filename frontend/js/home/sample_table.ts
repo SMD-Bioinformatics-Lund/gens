015476bb0363c625f81cea957bb2bbf42a6ec266
Jakob Willforss <jakob.willforss@hotmail.com>
2025-07-04 13:38:19 +0200

    Restore the case ID

--- frontend/js/home/sample_table.ts
+++ frontend/js/home/sample_table.ts
@@ -82,7 +82,7 @@ export class SamplesTable extends HTMLElement {
     this.tableContainer.hidden = false;
 
     const newRows = sampleInfo.map((s) => [
-      `<a href="${getGensURL(s.case_id)}">${s.case_id}</a> (<a href="${scoutBaseURL}/${s.case_id}" target="_blank" rel="noopener noreferrer">Scout</a>)`,
+      `<a href="${getGensURL(s.case_id)}">${s.case_id}</a> (<a href="${scoutBaseURL}/case/case_id/${s.case_id}" target="_blank" rel="noopener noreferrer">Scout</a>)`,
       s.sample_ids
         .map((id) => `<a href="${getGensURL(s.case_id, [id])}">${id}</a>`)
         .join(", "),
