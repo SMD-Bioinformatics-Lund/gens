b860014544815013226c2dee2c5b8bf27be3b0e7
Jakob Willforss <jakob.willforss@hotmail.com>
2025-09-02 14:25:17 +0200

    Move zoom steps to constant

--- frontend/js/constants.ts
+++ frontend/js/constants.ts
@@ -6,6 +6,12 @@ export const ZINDICES = {
   sideMenu: 2000,
 };
 
+export const ZOOM_STEPS = {
+  A: 50 * 10 ** 6,
+  B: 10 * 10 ** 6,
+  C: 500 * 10 ** 3,
+}
+
 // FIXME: This is hard-coded for the constitutional pipeline
 // Will need to consider how to generalize this for somatic (and micro?)
 export const DEFAULT_VARIANT_THRES = 14;
