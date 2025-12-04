20be482b1a437d7dccdc30b67a884e08bf2f1a97
Jakob Willforss <jakob.willforss@hotmail.com>
2025-12-04 13:44:39 +0100

    Allow running without interpretation software configured

--- gens/blueprints/gens/views.py
+++ gens/blueprints/gens/views.py
@@ -145,7 +145,7 @@ def display_samples(case_id: str):
 
     return render_template(
         "gens.html",
-        scout_base_url=settings.variant_url,
+        scout_base_url=str(settings.variant_url) if settings.variant_url else "",
         chrom=parsed_region.chromosome,
         start=parsed_region.start,
         end=parsed_region.end,
